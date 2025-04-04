c0927530272239b6405022bdd74478805997e60d
dimaeev <alexeeev.dima@gmail.com>
2025-04-04 15:36:33 +0700

    Полноценная программа

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text.RegularExpressions;
 
 class Program
@@ -10,19 +11,39 @@ class Program
     {
       { "првиет", "привет" },
       { "пирвет", "привет" },
-      { "здравсвуйте", "здравствуйте" }
+      { "здравсвуйте", "здравствуйте" },
+      { "спосибо", "спасибо" }
     };
 
+    string folderPath = Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).FullName).FullName;
+    string[] files = Directory.GetFiles(folderPath, "*.txt");
 
-    string inputText = "(097) 123-45-67";
+    foreach (string file in files)
+    {
+      string text = File.ReadAllText(file);
 
-    // Регулярное выражения для номеров
-    string pattern = @"\((\d{3})\)\s(\d{3})-(\d{2})-(\d{2})";
-    string replacement = "+380 $1 $2 $3 $4";
+      foreach (var mistake in mistakeDic)
+      {
+        text = text.Replace(mistake.Key, mistake.Value);
+      }
 
-    string result = Regex.Replace(inputText, pattern, replacement);
+      string pattern = @"\((\d{3})\)\s(\d{3})-(\d{2})-(\d{2})";
+
+      text = Regex.Replace(text, pattern, FormatPhoneNumber);
+
+      File.WriteAllText(file, text);
+
+      Console.WriteLine($"Исправления внесены в файл: {file}");
+    }
   }
 
+  static string FormatPhoneNumber(Match match)
+  {
+    string code = match.Groups[1].Value;
+    string part1 = match.Groups[2].Value;
+    string part2 = match.Groups[3].Value;
+    string part3 = match.Groups[4].Value;
 
-
+    return $"+380 {code.Substring(1)} {part1} {part2} {part3}";
+  }
 }

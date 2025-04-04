bb3adb33af4cca557958d1779a24c2cc60c1d355
dimaeev <alexeeev.dima@gmail.com>
2025-04-04 13:25:27 +0700

    Словарь ошибок

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Dictionary<string, string> mistakeDic = new Dictionary<string, string>()
+    {
+      { "првиет", "привет" },
+      { "пирвет", "привет" },
+      { "здравсвуйте", "здравствуйте" }
+    };
+  }
+
+
+
+}

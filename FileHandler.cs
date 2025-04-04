f72cf8cd79ba6d95319b7dc3c90f79052e35206c
dimaeev <alexeeev.dima@gmail.com>
2025-04-04 20:42:32 +0700

    Исправления

---
+++ FileHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+public class FileHandler
+{
+  private string folderPath;
+
+  public FileHandler(string path)
+  {
+    folderPath = path;
+  }
+
+  public string[] GetTextFiles()
+  {
+    return Directory.GetFiles(folderPath, "*.txt");
+  }
+
+  public string ReadFile(string filePath)
+  {
+    return File.ReadAllText(filePath);
+  }
+
+  public void WriteFile(string filePath, string content)
+  {
+    File.WriteAllText(filePath, content);
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,70 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text.RegularExpressions;
 
 class Program
 {
   static void Main(string[] args)
   {
-    string folderPath = GetFolderPath();
-    string[] files = GetTextFiles(folderPath);
+    string folderPath = Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).FullName).FullName;
+
+    FileHandler fileHandler = new FileHandler(folderPath);
+    TextProcessor textProcessor = new TextProcessor();
+
+    string[] files = fileHandler.GetTextFiles();
 
     foreach (string file in files)
     {
-      string text = File.ReadAllText(file);
+      string text = fileHandler.ReadFile(file);
 
-      text = CorrectMistakes(text);
+      text = textProcessor.CorrectMistakes(text);
+      text = textProcessor.FixPhoneNumbers(text);
 
-      text = FixPhoneNumbers(text);
-
-      File.WriteAllText(file, text);
+      fileHandler.WriteFile(file, text);
 
       Console.WriteLine($"Исправления внесены в файл: {file}");
     }
-  }
-
-  static string GetFolderPath()
-  {
-    return Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).FullName).FullName;
-  }
-
-  static string[] GetTextFiles(string folderPath)
-  {
-    return Directory.GetFiles(folderPath, "*.txt");
-  }
-
-  static string CorrectMistakes(string text)
-  {
-    Dictionary<string, string> mistakeDic = new Dictionary<string, string>()
-        {
-            { "првиет", "привет" },
-            { "пирвет", "привет" },
-            { "здравсвуйте", "здравствуйте" },
-            { "спосибо", "спасибо" }
-        };
-
-    foreach (var mistake in mistakeDic)
-    {
-      text = text.Replace(mistake.Key, mistake.Value);
-    }
-
-    return text;
-  }
-
-  static string FixPhoneNumbers(string text)
-  {
-    string pattern = @"\((\d{3})\)\s(\d{3})-(\d{2})-(\d{2})";
-    return Regex.Replace(text, pattern, FormatPhoneNumber);
-  }
-
-  static string FormatPhoneNumber(Match match)
-  {
-    string code = match.Groups[1].Value;
-    string part1 = match.Groups[2].Value;
-    string part2 = match.Groups[3].Value;
-    string part3 = match.Groups[4].Value;
-
-    return $"+380 {code.Substring(1)} {part1} {part2} {part3}";
   }
 }
